c20016d5a8ea94f6f840052494ac2124c1b4f572
Abdallah <abdallahchaar90@gmail.comh>
2021-01-09 17:49:40 +0100

    träning om array med pop och kan jag tagbort ett element from listan

---
+++ main.js
@@ -0,0 +1,49 @@
+/* global console, alert, prompt*/
+
+/*
+var friends = [
+    "mahmoud",
+    "Hassan",
+    "Soha",
+    "Ahmad",
+    "Zahra",
+    "Hind",
+    "Mohamad",
+    "samer"
+
+];
+
+console.log(friends);
+
+console.log(friends.length);
+
+friends.splice(3 ,4);
+
+console.log(friends);
+
+console.log(friends.length);
+*/
+
+var friends = [
+    "mahmoud",
+    "Hassan",
+    "Soha",
+    "Ahmad",
+    "Zahra",
+    "Hind",
+    "Mohamad",
+    "samer"
+
+];
+
+console.log(friends);
+
+console.log(friends.length);
+
+friends.pop();
+
+console.log(friends);
+
+console.log(friends.length);
+
+
